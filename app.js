fccab5c6d0fd8a61ba97f11347fbbc9e3de59966
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-03 18:08:14 -0600

    Estructura inicial del proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+const rojo = document.getElementById("rojo");
+const amarillo = document.getElementById("amarillo");
+const verde = document.getElementById("verde");
+
+function cambioRojo(){
+    rojo.style.backgroundColor = "rgba(217, 4, 41, 1)";
+}
+
+function cambioAmarillo() {
+    rojo.style.backgroundColor = "rgba(252, 243, 0, 1)";
+}
+
+function cambioVerde(){
+    rojo.style.backgroundColor = "rgba(36, 130, 50, 1)";
+}
+
+function intertaloTiempo() {
+
+}
